366f2fc3da55be4093168d3f0ebb575a3a629929
Anirudh <anirudh0288@gmail.com>
2022-11-16 12:33:36 +0530

    Instrumentalist Entry Updated

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "primeicons/primeicons.css";
 import ArtistForm from "./components/ArtistForm";
 import ContactUs from "./components/ContactUs";
+import ReactGA from "react-ga";
 
 function App() {
+  ReactGA.initialize("UA-232096677-1");
+  ReactGA.pageview(window.location.pathname + window.location.search);
   return (
     <>
       <Router basename={process.env.PUBLIC_URL}>
